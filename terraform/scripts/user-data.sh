2d984145e819f6ef22413558443b7d7eb2719dfd
Russell Groves <russellgroves@gmail.com>
2020-11-29 22:10:51 +0200

    Added jq to user-data

--- terraform/scripts/user-data.sh
+++ terraform/scripts/user-data.sh
@@ -29,3 +29,6 @@ sudo chkconfig docker on
 # Docker Compose
 sudo curl -L https://github.com/docker/compose/releases/download/1.21.0/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
+
+# JQ needed for bash scripts
+sudo yum install -y jq
