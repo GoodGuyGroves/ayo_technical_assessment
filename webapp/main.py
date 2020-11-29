9995d0e8ad2e17b0a1464c1fb20fd7faaf9a4424
Russell Groves <russellgroves@gmail.com>
2020-11-29 21:30:55 +0200

    Edited main.py to test webhook trigger

--- webapp/main.py
+++ webapp/main.py
@@ -7,3 +7,9 @@ app = FastAPI()
 async def root():
     """Root HTTP address"""
     return { "message": "Hello, world!" }
+
+# pylint: disable=invalid-name
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: str = None):
+    """Just returns the item_id and query parameter q"""
+    return {"item_id": item_id, "q": q}
